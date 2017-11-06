6a5dc1349113947e08dc521cf09894d348f9e51a
Ravi Shankar <wafflespeanut@gmail.com>
2017-11-06 16:06:57 +0530

    Setup callback-based FFI

--- Merileva/merileva.h
+++ Merileva/merileva.h
@@ -1,11 +1,36 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-// Create an opaque struct which mimics the Rust struct.
-struct naamio_service;
+// Opaque struct which mimics complex Swift/Rust structs.
+struct Opaque;
 
 // Create the Naamio client service.
-struct naamio_service *create_service(uint8_t threads);
+struct Opaque *create_service(uint8_t threads);
 
 // Since it's allocated on the Rust side, it should be deallocated there.
-void drop_service(struct naamio_service *ptr);
+void drop_service(struct Opaque *rust_service);
+
+// C-style struct to represent a String or a slice
+struct ByteArray {
+    const uint8_t *bytes;
+    size_t len;
+};
+
+// Foreign type owned by FFI realm (i.e., Swift classes).
+// Rust will call the destructor once it's utilized.
+struct ForeignObject {
+    void *object;
+    void (*destructor)(void *object);
+};
+
+/* Plugin registration */
+
+struct RegisterRequest {
+    struct ByteArray name;
+    struct ByteArray rel_url;
+    struct ByteArray endpoint;
+};
+
+void register_plugin(struct Opaque *rust_service,
+                     struct RegisterRequest *req,
+                     void (*callback)(struct ByteArray token));
--- NaamioClient.swift
+++ NaamioClient.swift
@@ -1,4 +1,57 @@
+import Foundation
 import Merileva
 
-let ptr = create_service(4)
-drop_service(ptr)
+extension ByteArray {
+    func asString() -> String? {
+        let ptr = UnsafeBufferPointer(start: bytes, count: len)
+        return String(bytes: ptr, encoding: String.Encoding.utf8)
+    }
+}
+
+extension String {
+    func asByteArray() -> ByteArray? {
+        guard let data = self.data(using: String.Encoding.utf8) else { return nil }
+
+        let buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: data.count)
+        let stream = OutputStream(toBuffer: buffer, capacity: data.count)
+
+        stream.open()
+        let _ = data.withUnsafeBytes({
+            stream.write($0, maxLength: data.count)
+        })
+
+        stream.close()
+        return ByteArray(bytes: buffer, len: data.count)
+    }
+}
+
+class NaamioService {
+    private let ptr: OpaquePointer
+
+    init(threads: UInt8) {
+        ptr = create_service(threads)
+    }
+
+    deinit {
+        drop_service(ptr)
+    }
+
+    func registerPlugin(name: String, relUrl: String, endpoint: String) {
+        if let name = name.asByteArray(),
+           let relUrl = relUrl.asByteArray(),
+           let endpoint = endpoint.asByteArray()
+        {
+            var req = RegisterRequest(name: name, rel_url: relUrl, endpoint: endpoint)
+            register_plugin(ptr, &req, { (token) in
+                if let token = token.asString() {
+                    print("\(token)")
+                }
+            })
+        }
+    }
+}
+
+let service = NaamioService(threads: 4)
+service.registerPlugin(name: "foo", relUrl: "/hey", endpoint: "localhost")
+
+sleep(2)
